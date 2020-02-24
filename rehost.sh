6c9405339f060e465ead8ff15a8f91ef4ba7c0f3
warrenc5 <warren@mofokom.biz>
2020-02-25 06:31:38 +1100

    run arpd for 10 seconds on all interfaces

--- rehost.sh
+++ rehost.sh
@@ -1,7 +1,29 @@
 #!/bin/bash -x
 HOSTS=/etc/hosts
 HOST_ETHERS=/etc/hosts-ethers
-arpd -l |grep -v '#'  | grep -v FAILED | sort -k 3 > /tmp/arp.txt
+ARP_DB=/var/tmp/arpd.db
+
+arpd -b $ARP_DB -a 3 -k `ifconfig -s | cut -f 1 -d ' ' | grep -v Iface`
+
+ARP_PID=$(pgrep arpd)
+
+sleep 10
+kill $ARP_PID
+
+arpd -l -b $ARP_DB | grep -v '#'  | grep -v FAILED | sort -k 3 > /tmp/arp.txt
+
+if [ ! -s /tmp/arp.txt ] ; then 
+	echo '/tmp/arp.txt is empty - no arps?' 
+	exit 1
+fi
+#ifconfig -s | cut -f 1 -d ' ' | grep -v Iface
+
+if [ ! -f ${HOST_ETHERS} ] ; then
+	echo "create $HOST_ETHERS"
+	echo "[hostname] [mac]"
+
+	exit 1
+fi
 
 sort -k 2 $HOST_ETHERS > /tmp/hosts-ethers
 join -1 3 -2 2 /tmp/arp.txt /tmp/hosts-ethers | cut -d ' ' -f 3-4  | sort -k 1 > /tmp/hosts
@@ -15,5 +37,11 @@ while IFS= read -r line; do
     IP="${BASH_REMATCH[1]}"
     NAME="${BASH_REMATCH[2]}"
     echo "-${IP}-${NAME}-"
-    sed -i "/$NAME/ s/^.* /$IP /" $HOSTS
+    grep $NAME $HOSTS
+    if [ $? == 0 ] ; then
+	    sed -i "/$NAME/ s/^.* /$IP /" $HOSTS
+    else 
+	    echo "$IP	$NAME" >> $HOSTS
+    fi
+
 done < /tmp/hosts
